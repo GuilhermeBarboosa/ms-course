faea4cd4bde730d930478d3849c1537da6217e94
Guilherme Barbosa <78804519+GuilhermeBarboosa@users.noreply.github.com>
2024-07-24 13:45:53 -0300

    feat feign client

--- hr-payroll/src/main/java/com/codiub/hr_payroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/codiub/hr_payroll/service/PaymentService.java
@@ -1,5 +1,6 @@
 package com.codiub.hr_payroll.service;
 
+import com.codiub.hr_payroll.config.WorkerFeignClient;
 import com.codiub.hr_payroll.entity.Payment;
 import com.codiub.hr_payroll.entity.Worker;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +14,21 @@ import java.util.Map;
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
+//    @Value("${hr-worker.host}")
+//    private String workerHost;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
+
+//    @Autowired
+//    private RestTemplate restTemplate;
 
     public Payment getPayment(Long workerId, Integer days) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", workerId.toString());
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+//        Map<String, String> uriVariables = new HashMap<>();
+//        uriVariables.put("id", workerId.toString());
+//        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
