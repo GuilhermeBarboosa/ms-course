ca815b1cf6b503bd1f7771c220c8d173354e8d49
Guilherme Barbosa <78804519+GuilhermeBarboosa@users.noreply.github.com>
2024-07-25 08:59:24 -0300

    feat config server profile

--- hr-worker/src/main/java/com/codiub/hr_worker/resource/WorkerResource.java
+++ hr-worker/src/main/java/com/codiub/hr_worker/resource/WorkerResource.java
@@ -5,6 +5,7 @@ import com.codiub.hr_worker.repository.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,11 +19,22 @@ public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private Environment env;
 
     @Autowired
     private WorkerRepository repository;
+
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> configs() {
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
+    }
+
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
